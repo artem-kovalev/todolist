43991280ad4b3683933c0bd94bcd907ea75e6a69
Artem Kovalev <nomit007@gmail.com>
2018-10-15 21:39:58 +0700

    Add: Default jwt setting

--- src/Todolist.Service.Auth/Startup.cs
+++ src/Todolist.Service.Auth/Startup.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using System.Transactions;
 using AspNetCore.Identity.Mongo;
 using AspNetCore.Identity.Mongo.Model;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -13,6 +16,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using MongoDB.Driver;
 using Todolist.Service.Auth.Configuration;
 using Todolist.Service.Auth.Models;
 
@@ -31,7 +36,8 @@ namespace Todolist.Service.Auth
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-            services.AddIdentityMongoDbProvider<User, Role>(settings =>
+            services.AddIdentityMongoDbProvider<User, Role>(
+                settings =>
                 {
                     settings.Password.RequireNonAlphanumeric = false;
                     settings.Password.RequireUppercase = false;
@@ -41,6 +47,25 @@ namespace Todolist.Service.Auth
                     var connection = Configuration.GetConnectionString("MongoDb");
                     options.ConnectionString = connection;
                 });
+            services.AddAuthentication(options =>
+                {
+                    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                })
+                .AddJwtBearer(options =>
+                {
+                    options.RequireHttpsMetadata = false;
+                    options.SaveToken = true;
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes("abc")),
+                        ValidateIssuer = false,
+                        ValidateAudience = false
+                    };
+                });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -54,6 +79,7 @@ namespace Todolist.Service.Auth
             {
                 app.UseHsts();
             }
+
             roleManager.SeedRoles();
             userManager.SeedUsers();
 
